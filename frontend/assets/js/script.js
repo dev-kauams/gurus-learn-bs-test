51ded448eebc471d18c5edb47d9005158ec3e8bf
dev-kauams <kauams.profissional@gmail.com>
2026-04-30 11:33:29 -0300

    feat: :sparkles: Complete Login Page Style
    login page style, with library gif and new inputs in register form.

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,11 +1,7 @@
 const registerLink = document.getElementById('register_link');
-const loginLink = document.getElementById('login_link');
+
 const cardLogin = document.querySelector('.card-login');
 
 registerLink.addEventListener('click', () => {
-    cardLogin.classList.add('show-register');
-});
-
-loginLink.addEventListener('click', () => {
-    cardLogin.classList.remove('show-register');
+    cardLogin.classList.toggle('show-register');
 });
